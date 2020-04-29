4a03c2f566c1ed2fd88a9b53c1c7eca3fd252d3b
adv7 <cud4xx@gmail.com>
2020-04-29 22:46:03 +0200

    pointers training

--- BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
+++ BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "gameframework/actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,10 +20,10 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("This is WARNING!"));
-	UE_LOG(LogTemp, Error, TEXT("This is ERROR!"));
-	UE_LOG(LogTemp, Display, TEXT("This is DISPLAY!"));
-	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at position %s"), *ObjectName, *ObjectPosition);
 }
 
 
